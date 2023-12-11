ba8cfb1cc0eb2c0e31a8e5334c1ea9d981be0e7b
CharlotteBorie <lilou.borie@gmail.com>
2023-12-11 22:39:06 +0100

    improve Day 8 part 2

--- src/Day8/exercice.rs
+++ src/Day8/exercice.rs
@@ -1,117 +1,145 @@
-use std::collections::HashMap;
 use itertools::process_results;
+use std::collections::HashMap;
 
-pub fn part1(){
+pub fn part1() {
     let input = include_str!("Input.txt");
     println!("Day 8 Part 1");
     println!("{:?}", part1_implementation(input));
 }
 
-pub fn part2(){
+pub fn part2() {
     let input = include_str!("Input.txt");
     println!("Day 8 Part 2");
     println!("{:?}", part2_implementation(input));
 }
 
-pub fn bench(){
+pub fn bench() {
     let input = include_str!("Input.txt");
     part1_implementation(input);
     part2_implementation(input);
 }
-pub fn bench1(){
+pub fn bench1() {
     let input = include_str!("Input.txt");
     part1_implementation(input);
 }
-pub fn bench2(){
+pub fn bench2() {
     let input = include_str!("Input.txt");
     part2_implementation(input);
 }
-
-
 
 pub fn part1_implementation(input: &str) -> String {
     let mut count = 0;
     let instruction = get_instruction(input);
     let dictionary = get_dictionary(input);
 
-    get_path_length_from_start(&instruction, & dictionary, "AAA",|current| get_end(current,true)).to_string()
+    get_path_length_from_start(&instruction, &dictionary, "AAA", |current| {
+        get_end(current, true)
+    })
+    .to_string()
 }
-
 
 pub fn part2_implementation(input: &str) -> String {
     let mut count = 0;
     let instruction = get_instruction(input);
     let dictionary = get_dictionary(input);
-    let list_currents = dictionary.keys().clone().filter(|x| x.chars().last() == Some('A')).collect::<Vec<&&str>>();
-    let counts = list_currents.iter().map(|x| get_path_length_from_start(&instruction, & dictionary, x,|current| get_end(current,false))).collect::<Vec<u64>>();
-    counts.iter().fold(counts[0], |acc, &x| get_lcm(acc, x)).to_string()
+    let list_currents = dictionary
+        .keys()
+        .clone()
+        .filter(|x| x.chars().last() == Some('A'))
+        .collect::<Vec<&&str>>();
+    let counts = list_currents
+        .iter()
+        .map(|x| {
+            get_path_length_from_start(&instruction, &dictionary, x, |current| {
+                get_end(current, false)
+            })
+        })
+        .collect::<Vec<u64>>();
+    counts
+        .iter()
+        .fold(counts[0], |acc, &x| get_lcm(acc, x))
+        .to_string()
 }
 
 fn get_instruction(input: &str) -> Vec<Instruction> {
-    let instruction = input.lines().next().unwrap().chars().map(|x| match x {
-        'L' => Instruction::Left,
-        'R' => Instruction::Right,
-        _ => panic!("Invalid instruction"),
-    }).collect::<Vec<Instruction>>();
+    let instruction = input
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .map(|x| match x {
+            'L' => Instruction::Left,
+            'R' => Instruction::Right,
+            _ => panic!("Invalid instruction"),
+        })
+        .collect::<Vec<Instruction>>();
     instruction
 }
 fn get_dictionary(input: &str) -> HashMap<&str, (&str, &str)> {
-    let mut dictionnary: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut dictionnary: HashMap<&str, (&str, &str)> = HashMap::with_capacity(1024);
     for (index, line) in input.lines().enumerate() {
         if index <= 1 {
             continue;
         }
-        let split = line.split([' ', '(', ',', ')', '=']).filter(|x| !x.is_empty()).collect::<Vec<&str>>();
+        let split = line
+            .split([' ', '(', ',', ')', '='])
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<&str>>();
         dictionnary.insert(split[0], (split[1], split[2]));
     }
     dictionnary
 }
 
-fn get_path_length_from_start(instruction: &Vec<Instruction>, dictionary: & HashMap<&str, (&str, &str)>, start: &str, end : fn(&str) -> bool) -> u64 {
+fn get_path_length_from_start(
+    instruction: &Vec<Instruction>,
+    dictionary: &HashMap<&str, (&str, &str)>,
+    start: &str,
+    end: fn(&str) -> bool,
+) -> u64 {
     let mut count = 0;
-    let mut instruction_index = 0;
     let mut current = start;
+    let mut instruction_pattern = instruction.iter();
     while end(&current) {
         let (left, right) = dictionary.get(current).unwrap();
-        let current_instruction = &instruction[instruction_index];
-        current = match &current_instruction {
-            Instruction::Left => left,
-            Instruction::Right => right,
+        current = match instruction_pattern.next() {
+            Some(Instruction::Left) => left,
+            Some(Instruction::Right) => right,
+            _ => {
+                instruction_pattern = instruction.iter();
+                continue;
+            }
         };
         count += 1;
-        instruction_index += 1;
-        if instruction_index == instruction.len() {
-            instruction_index = 0;
-        }
     }
     count
 }
 
-fn get_end(current: &str,part1 : bool) -> bool {
+fn get_end(current: &str, part1: bool) -> bool {
     if part1 {
-        return current != "ZZZ"
+        return current != "ZZZ";
     }
     current.chars().last() != Some('Z')
 }
 
-pub fn get_lcm(a: u64 , b: u64) -> u64 {
-    let mut current_a = a;
-    let mut current_b = b;
-    while current_a != current_b {
-        if current_a > current_b {
-            current_b += b;
-        } else {
-            current_a += a;
-        }
+fn get_lcm(a: u64, b: u64) -> u64 {
+    a * b / get_gcd(a, b)
+}
+
+fn get_gcd(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
     }
-    current_a
+    a
 }
 
 pub enum Instruction {
     Left,
     Right,
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -124,8 +152,6 @@ mod tests {
 
         let input = include_str!("Example2.txt");
         assert_eq!(part1_implementation(input), 6.to_string());
-
-
     }
 
     #[test]
